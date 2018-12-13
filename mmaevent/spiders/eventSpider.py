8d9f37b02df0023b2953d524c62d724c5889f537
Brian Trang <briantrang@Brians-MacBook-Air.local>
2018-12-13 13:32:06 -0800

    removed newlines from fighter records

--- mmaevent/spiders/eventSpider.py
+++ mmaevent/spiders/eventSpider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+#run file by 'scrapy crawl <name_of_the_spider> -o <output_file_name>.json'
 class EventSpider(scrapy.Spider):
     name = "event"  #every spider needs a name
 
@@ -12,8 +12,16 @@ class EventSpider(scrapy.Spider):
             yield {
                 "number": row.css("div.fightCardBoutNumber::text").extract_first(),
                 "nameleft": row.css("div.fightCardFighterName.left a::text").extract_first(),
-                "recordright":row.css("div.right div.fightCardRecord::text").extract_first(),
-                "recordleft": row.css("div.fightCardFighterBout.left div.fightCardRecord::text").extract_first(),
+                "recordright": ''.join(row.css('div.fightCardFighterBout.right div.fightCardRecord::text').getall()).strip(),
+                "recordleft": ''.join(row.css("div.fightCardFighterBout.left div.fightCardRecord::text").getall()).strip(),
                 "nameright": row.css("div.fightCardFighterName.right a::text").extract_first()      
             }
             #recordright is only grabbing the \n.....
+            #''.join(row.css('div.fightCardFighterBout.right div.fightCardRecord::text').getall()).strip()
+
+
+
+            #Resources
+            #https://www.reddit.com/r/scrapy/comments/a5gs19/first_time_using_scrapy_cant_seem_to_scrape_mma/ebnmiv7/?context=3
+            #https://medium.com/@mottet.dev/scrapy-and-scrapyrt-how-to-create-your-own-api-from-almost-any-website-ecfb0058ad64
+            
